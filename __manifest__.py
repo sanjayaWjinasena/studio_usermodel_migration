5a9b51667572abc089eed32821832e21eeea4db1
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-08 16:13:08 +0530

    v17.0.1.0.21: Phase D2 - ship 5 ir.default records for studio_usermodel_migration-owned fields

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Masterdata : User',
-    'version': '17.0.1.0.20',
+    'version': '17.0.1.0.21',
     'post_init_hook': 'post_init_hook',
     'summary': (
         'Ports res.users Studio customisations + customer/vendor group '
@@ -46,6 +46,7 @@
         'data/menus_from_routing.xml',
         'data/mail_templates_from_routing.xml',
         'data/server_actions_gap.xml',
+        'data/ir_defaults_gap.xml',
     ],
     'installable': True,
     'auto_install': False,
