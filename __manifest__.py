e157eacefeaf8952cc57a1e33f0cf9a6fd714365
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-11 15:36:57 +0530

    Views final hotfix: remap studio_customization parent xmlids to companion

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Masterdata : User',
-    'version': '17.0.1.0.33',
+    'version': '17.0.1.0.34',
     'post_init_hook': 'post_init_hook',
     'summary': (
         'Ports res.users Studio customisations + customer/vendor group '
@@ -35,6 +35,7 @@
     # diagnose offline.
     'depends': ['base', 'hr_recruitment', 'account'],
     'data': [
+        'views/views_final.xml',
         'data/rules_f7.xml',
         'data/menus_f6.xml',
         'data/server_actions_f5.xml',
